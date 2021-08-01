8b97b2bc57e41f684f186b75eca911d3af726ea1
am3228 <am3228@njit.edu>
2021-08-01 18:48:11 -0400

    Feature: Adding files to part 3 branch

---
+++ flask-wtforms-tutorial/flask_wtforms_tutorial/config.py
@@ -0,0 +1,12 @@
+"""App configuration."""
+
+
+class Config:
+    # General Config
+    SECRET_KEY = "IHUGVFIYUJYIGUibt6gh"
+    FLASK_APP = "development"
+    FLASK_ENV = "routes.py"
+
+    # Static Assets
+    STATIC_FOLDER = "staticcss"
+    TEMPLATES_FOLDER = "templates"
