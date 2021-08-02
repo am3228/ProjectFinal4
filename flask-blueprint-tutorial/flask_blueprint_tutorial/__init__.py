42d259b8900cc34df8a8b1d3b3c3252e32750990
am3228 <am3228@njit.edu>
2021-08-02 12:34:36 -0400

    Feature: Registering multiple blueprints step 7

--- flask-blueprint-tutorial/flask_blueprint_tutorial/__init__.py
+++ flask-blueprint-tutorial/flask_blueprint_tutorial/__init__.py
@@ -9,9 +9,13 @@ def create_app():
 
     with app.app_context():
         # Import parts of our application
+        from .profile import routes
         from .home import routes
+        from .products import routes
 
         # Register Blueprints
+        app.register_blueprint(profile.account_bp)
         app.register_blueprint(home.home_bp)
+        app.register_blueprint(products.product_bp)
 
         return app
