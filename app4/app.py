0e0dc4f3214086cff44d89398874b5a434a59373
am3228 <am3228@njit.edu>
2021-08-01 20:32:02 -0400

    Feature: rearranging app file for Part 4

--- app4/app.py
+++ app4/app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template, make_response, redirect, url_for
+from flask import Flask, render_template, make_response, redirect, url_for, request
+from forms import SignupForm
+from flask import g
 
 app = Flask(
     __name__,
-    template_folder="templates"
+    template_folder="templates",
+    static_folder="static",
+    static_url_path=''
 )
+app.config.from_object('config.Config')
+
+@app.route("/home")
+def home():
+    return "Hello World!"
+
+@app.route("/api/v1/users/", methods=['GET', 'POST', 'PUT'])
+def users():
+    #... Logic goes here
+    return "This API endpoint accepts GET, POST, or PUT requests"
+
+@app.route('/user/,username')
+def profile(username):
+    #... Logic goes here
+    return "Hello, " + str(username)
 
 @app.route("/")
 def home():
@@ -27,13 +46,43 @@ def users():
 def login():
     return redirect(url_for('dashboard')
 
-@app.route("/api/v1/users/", methods=['GET', 'POST', 'PUT'])
-def users():
-    #... Logic goes here
-
-@app.route('/user/,username')
-def profile(username):
-    #... Logic goes here
+@app.route('/signup', methods=['GET', 'POST'])
+    def signup_page():
+        """User sign-up page."""
+        signup_form = SignupForm(request.form)
+        # POST: Sign user in
+        if request.method == 'POST':
+            if signup_form.validate():
+                # Get Form Fields
+                name = request.form.get('name')
+                email = request.form.get('email')
+                password = request.form.get('password')
+                website = request.form.get('website')
+                existing_user = User.query.filter_by(email=email).first()
+                if existing_user is None:
+                    user = User(
+                        name=name,
+                        email=email,
+                        password=generate_password_hash(
+                            password,
+                            method='sha256'
+                        ),
+                        website=website
+                    )
+                    db.session.add(user)
+                    db.session.commit()
+                    login_user(user)
+                    return redirect(url_for('main_bp.dashboard'))
+                flash('A user exists with that email address.')
+                return redirect(url_for('auth_bp.signup_page'))
+        # GET: Serve Sign-up page
+        return render_template(
+            '/signup.html',
+            title='Create an Account | Flask-Login Tutorial.',
+            form=SignupForm(),
+            template='signup-page',
+            body="Sign up for a user account."
+        )
 
 @app.route('/<int:year>/<int:month>/<title>')
 def article(year, month, title):
