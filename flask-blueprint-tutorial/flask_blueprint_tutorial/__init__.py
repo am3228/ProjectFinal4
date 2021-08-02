1a3fcd4b5ab72f3d298c0343a62a1d9eb37c1735
am3228 <am3228@njit.edu>
2021-08-02 12:50:27 -0400

    Feature: Adjustments to init file for Part 7

--- flask-blueprint-tutorial/flask_blueprint_tutorial/__init__.py
+++ flask-blueprint-tutorial/flask_blueprint_tutorial/__init__.py
@@ -1,21 +1,32 @@
 """Initialize Flask app."""
 from flask import Flask
+from flask_assets import Environment
+from ddtrace import patch_all
 
 
-def create_app():
+patch_all()
+
+
+def init_app():
     """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object("config.Config")
+    assets = Environment()
+    assets.init_app(app)
 
     with app.app_context():
         # Import parts of our application
-        from .profile import routes
-        from .home import routes
-        from .products import routes
+        from .assets import compile_static_assets
+        from .home import home
+        from .products import products
+        from .profile import profile
 
         # Register Blueprints
-        app.register_blueprint(profile.account_bp)
+        app.register_blueprint(profile.profile_bp)
         app.register_blueprint(home.home_bp)
         app.register_blueprint(products.product_bp)
+
+        # Compile static assets
+        compile_static_assets(assets)
 
         return app
