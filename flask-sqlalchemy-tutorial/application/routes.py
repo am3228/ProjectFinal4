0344da7f6da68c3325f5259d3b545a54975c8e1a
am3228 <am3228@njit.edu>
2021-08-02 18:15:34 -0400

    Feature: Querying Our New Data Part 9

--- flask-sqlalchemy-tutorial/application/routes.py
+++ flask-sqlalchemy-tutorial/application/routes.py
@@ -1,15 +1,16 @@
-from flask import request, render_template, make_response
-from datetime import datetime as dt
-from flask import current_app as app
-from .models import db, User
-
-
 @app.route('/', methods=['GET'])
 def user_records():
     """Create a user via query string parameters."""
     username = request.args.get('user')
     email = request.args.get('email')
     if username and email:
+        existing_user = User.query.filter(
+            User.username == username or User.email == email
+        ).first()
+        if existing_user:
+            return make_response(
+                f'{username} ({email}) already created!'
+            )
         new_user = User(
             username=username,
             email=email,
@@ -17,7 +18,12 @@ def user_records():
             bio="In West Philadelphia born and raised, \
             on the playground is where I spent most of my days",
             admin=False
-        )
+        )  # Create an instance of the User class
         db.session.add(new_user)  # Adds new User record to database
         db.session.commit()  # Commits all changes
-    return make_response(f"{new_user} successfully created!")
+        redirect(url_for('user_records'))
+    return render_template(
+        'users.jinja2',
+        users=User.query.all(),
+        title="Show Users"
+    )
