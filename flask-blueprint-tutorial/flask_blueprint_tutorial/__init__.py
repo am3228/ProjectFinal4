0d5fb996529da3701e29cc0f167ed18078a68cbb
am3228 <am3228@njit.edu>
2021-08-02 14:09:20 -0400

    Feature: Editing on part 7 branch

--- flask-blueprint-tutorial/flask_blueprint_tutorial/__init__.py
+++ flask-blueprint-tutorial/flask_blueprint_tutorial/__init__.py
@@ -1,32 +1,28 @@
 """Initialize Flask app."""
 from flask import Flask
-from flask_assets import Environment
-from ddtrace import patch_all
+from flask_assets import Environment  # <-- Import `Environment`
 
 
-patch_all()
-
-
-def init_app():
+def create_app():
     """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object("config.Config")
-    assets = Environment()
-    assets.init_app(app)
+    app.config.from_object('config.Config')
+    assets = Environment()  # Create an assets environment
+    assets.init_app(app)  # Initialize Flask-Assets
 
     with app.app_context():
         # Import parts of our application
-        from .assets import compile_static_assets
+        from .profile import profile
         from .home import home
         from .products import products
-        from .profile import profile
+        from .assets import compile_static_assets
 
         # Register Blueprints
-        app.register_blueprint(profile.profile_bp)
+        app.register_blueprint(profile.account_bp)
         app.register_blueprint(home.home_bp)
         app.register_blueprint(products.product_bp)
 
         # Compile static assets
-        compile_static_assets(assets)
+        compile_static_assets(assets)  # Execute logic
 
         return app
