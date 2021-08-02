44d4f005fe899f8a4e7647f00b00ce84e5e1165f
am3228 <am3228@njit.edu>
2021-08-01 20:57:53 -0400

    Feature: Rearranging app file to run

--- app4/app.py
+++ app4/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, make_response, redirect, url_for, requ
 from forms import SignupForm
 from flask import g
 
+
 app = Flask(
     __name__,
     template_folder="templates",
@@ -10,33 +11,39 @@ app = Flask(
 )
 app.config.from_object('config.Config')
 
+
 @app.route("/home")
 def home():
     return "Hello World!"
 
+
 @app.route("/api/v1/users/", methods=['GET', 'POST', 'PUT'])
 def users():
-    #... Logic goes here
+    # ... Logic goes here
     return "This API endpoint accepts GET, POST, or PUT requests"
 
-@app.route('/user/,username')
+
+@app.route('/user/<username>')
 def profile(username):
-    #... Logic goes here
+    # ... Logic goes here
     return "Hello, " + str(username)
+
 
 @app.route('/<int:year>/<int:month>/<title>')
 def article(year, month, title):
-    #... Logic goes here
+    # ... Logic goes here
     return "Article Title: " + str(title) + "<br/> Month: " + str(month) + "<br/> Year: " + str(year)
 
+
 @app.route("/")
-def home():
+def index():
     """Serve homepage template."""
     return render_template(
-        'index.html',
-        title='Flask-Login Tutorial.',
-        body="You are now logged in!"
+        "index.html",
+        title='Flask Tutorial: Part 4',
+        body='This is from a template in Part 4 of the Flask Tutorial'
     )
+
 
 @app.route("/api/v2/test_response")
 def api_users():
@@ -44,51 +51,56 @@ def api_users():
     response.headers['Content-Type'] = 'application/json'
     return response
 
+
 @app.route("/signin")
 def dashboard():
+    # This had to serve a static page b/c of how tutorial made the route
     return redirect('/dashboard.html')
+
 
 @app.route("/login")
 def login():
-    return redirect(url_for('dashboard')
+    return redirect(url_for('dashboard'))
 
-@ app.route('/signup', methods=['GET', 'POST'])
-    def signup_page():
-        """User sign-up page."""
-        # signup_form = SignupForm(request.form)
-        # POST: Sign user in
-        # if request.method == 'POST':
-        #     if signup_form.validate():
-        #         # Get Form Fields
-        #         name = request.form.get('name')
-        #         email = request.form.get('email')
-        #         password = request.form.get('password')
-        #         website = request.form.get('website')
-        #         existing_user = User.query.filter_by(email=email).first()
-        #         if existing_user is None:
-        #             user = User(
-        #                 name=name,
-        #                 email=email,
-        #                 password=generate_password_hash(
-        #                     password,
-        #                     method='sha256'
-        #                 ),
-        #                 website=website
-        #             )
-        #             db.session.add(user)
-        #             db.session.commit()
-        #             login_user(user)
-        #             return redirect(url_for('main_bp.dashboard'))
-        #         flash('A user exists with that email address.')
-        #         return redirect(url_for('auth_bp.signup_page'))
-        # GET: Serve Sign-up page
-        return render_template(
-            '/signup.html',
-            title='Create an Account | Flask-Login Tutorial.',
-            form=SignupForm(),
-            template='signup-page',
-            body="Sign up for a user account."
-        )
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup_page():
+    """User sign-up page."""
+    # signup_form = SignupForm(request.form)
+    # POST: Sign user in
+    # if request.method == 'POST':
+    #     if signup_form.validate():
+    #         # Get Form Fields
+    #         name = request.form.get('name')
+    #         email = request.form.get('email')
+    #         password = request.form.get('password')
+    #         website = request.form.get('website')
+    #         existing_user = User.query.filter_by(email=email).first()
+    #         if existing_user is None:
+    #             user = User(
+    #                 name=name,
+    #                 email=email,
+    #                 password=generate_password_hash(
+    #                     password,
+    #                     method='sha256'
+    #                 ),
+    #                 website=website
+    #             )
+    #             db.session.add(user)
+    #             db.session.commit()
+    #             login_user(user)
+    #             return redirect(url_for('main_bp.dashboard'))
+    #         flash('A user exists with that email address.')
+    #         return redirect(url_for('auth_bp.signup_page'))
+    # GET: Serve Sign-up page
+    return render_template(
+        '/signup.html',
+        title='Create an Account | Flask-Login Tutorial.',
+        form=SignupForm(),
+        template='signup-page',
+        body="Sign up for a user account."
+    )
+
 
 @app.route("/g")
 def get_test_value():
@@ -96,12 +108,14 @@ def get_test_value():
         g.test_value = 'This the g-value'
     return g.test_value
 
+
 @app.route("/rm/g")
 def rm_test_value():
     if 'test_value' in g:
         redirect(url_for('remove_test_value'))
     else:
         return "There is NO g-value"
+
 
 # @app.teardown_testvalue
 def remove_test_value():
@@ -134,6 +148,6 @@ def server_error(arg):
         # 500
     # )
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', post=5003, debug=True)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5003, debug=True)
