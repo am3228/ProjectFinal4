c9e1df3aab4daa5b7e8a419f9e0738ebdc791e13
am3228 <am3228@njit.edu>
2021-08-02 12:31:52 -0400

    Feature: Configuring blueprint Step 7

---
+++ flask-blueprint-tutorial/flask_blueprint_tutorial/home/routes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from flask import current_app as app
+
+
+# Blueprint Configuration
+home_bp = Blueprint(
+    'home_bp', __name__,
+    template_folder='templates',
+    static_folder='static'
+)
