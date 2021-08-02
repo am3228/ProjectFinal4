26ac6b7cfcad14f019605895530146b9fe9fba51
am3228 <am3228@njit.edu>
2021-08-02 17:18:00 -0400

    Feature: Initalizing flask environments Part 8

--- app8/app.py
+++ app8/app.py
@@ -5,3 +5,16 @@ from flask_assets import Environment
 app = Flask(__name__, instance_relative_config=False)
 
 assets = Environment(app)
+
+style_bundle = Bundle(
+    'src/less/*.less',
+    filters='less,cssmin',
+    output='dist/css/style.min.css',
+    extra={'rel': 'stylesheet/css'}
+)
+
+# Register style bundle
+assets.register('main_styles', style_bundle)
+
+# Build LESS styles
+style_bundle.build()
